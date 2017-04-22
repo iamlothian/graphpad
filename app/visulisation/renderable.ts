740148efd94615bbe1c766771585cd815870307d
Matthew Lothian <lothian.studios@gmail.com>
2017-04-22 23:31:31 +1000

    added update to Renderabe interface with comments

--- app/visulisation/renderable.ts
+++ app/visulisation/renderable.ts
@@ -2,6 +2,14 @@ import {SimulationNodeDatum} from 'd3-force';
 
 export interface Renderable {
 
+    /**
+     * stage a simulation datum. Shoulde be called when data changes.
+     */
+    update(nodes:SimulationNodeDatum):void;
+
+    /**
+     * render a simulation datum. Should be called for every tick of the simulation.
+     */
     draw(sprite:SimulationNodeDatum): void;
 
 }
